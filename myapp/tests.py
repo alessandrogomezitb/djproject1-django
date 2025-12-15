32d8bee67d40e4816f081019f648feabffb358ee
Alessandro <mario.moncada.7e9@itb.cat>
2025-12-15 22:03:59 +0100

    testdb

--- myapp/tests.py
+++ myapp/tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
  
 class MySeleniumTests(StaticLiveServerTestCase):
     #carregar una BD de test
-    #fixtures = ['testdb.json',]
+    fixtures = ['testdb.json',]
  
     @classmethod
     def setUpClass(cls):
@@ -18,7 +18,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
     def tearDownClass(cls):
         # tanquem browser
         # comentar la propera línia si volem veure el resultat de l'execució al navegador
-        #cls.selenium.quit()
+        cls.selenium.quit()
         super().tearDownClass()
  
     def test_login(self):
@@ -38,3 +38,19 @@ class MySeleniumTests(StaticLiveServerTestCase):
         # testejem que hem entrat a l'admin panel comprovant el títol de la pàgina
         self.assertEqual( self.selenium.title , "Site administration | Django site admin" )
 
+    def test_login_error(self):
+        # comprovem que amb un usuari i contrasenya inexistent, el test falla
+        self.selenium.get(f'{self.live_server_url}/admin/login/')
+        self.assertEqual(self.selenium.title, "Log in | Django site admin")
+        
+        # introduim dades de login
+        username_input = self.selenium.find_element(By.NAME, "username")
+        username_input.send_keys('usuari_no_existent')
+        
+        password_input = self.selenium.find_element(By.NAME, "password")
+        password_input.send_keys('contrasenya_incorrecta')
+        
+        self.selenium.find_element(By.XPATH, '//input[@value="Log in"]').click()
+        
+        # utilitzem assertNotEqual per testejar que NO hem entrat
+        self.assertNotEqual(self.selenium.title, "Site administration | Django site admin")
